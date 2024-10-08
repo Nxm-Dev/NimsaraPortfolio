d565d33b001df5b0e8041f3588b2bec231e21deb
N3mxaraMe-CodeArc <Nimsaraofficial@gmail.com>
2024-10-08 23:52:13 +0530

    refactor: Update landing page with improved header and about section
    The code changes include updating the landing page header with a new logo and menu icon, adding a "Schedule a Meet" button, and enhancing the about section with a more detailed description of the developer's skills and experience.

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let section = document.querySelectorAll('section');
 let navlink = document.querySelectorAll('header nav a');
+let menuIcon = document.querySelector('#menu-icon');
+let nav = document.querySelector('.navbar');
 
+// Handle scroll events
 window.onscroll = () => {
     section.forEach(sec => {
         let top = window.scrollY;
@@ -12,41 +15,35 @@ window.onscroll = () => {
             navlink.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            })
+            });
         }
-    })
+    });
 
-
-    // toggle menu
-    let menuIcon = document.querySelector('#menu-icon');
-    let nav = document.querySelector('.navbar');
-
-
-
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        nav.classList.toggle('active');
-    }
-
-
-
-
+    // Make header sticky after scrolling past 100px
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+};
 
-    // remove toggle icon Nav bar when click nav bar link
+// Handle menu toggle
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    nav.classList.toggle('active');
+};
 
-    menuIcon.classList.remove('bx-x');
-    nav.classList.remove('active');
-}
+// Close the menu when a nav link is clicked
+navlink.forEach(link => {
+    link.addEventListener('click', () => {
+        menuIcon.classList.remove('bx-x');
+        nav.classList.remove('active');
+    });
+});
 
-// Scrolling Changes
-
+// ScrollReveal configurations
 ScrollReveal({
     distance: '80px',
     duration: 2000,
     delay: 50
-})
+});
 
 ScrollReveal().reveal('.home-content,.heading ,.portfolio-box2', { origin: 'top' });
 ScrollReveal().reveal('.services-container ,.about-content, .contact form ,.portfolio-box5', { origin: 'bottom' });
