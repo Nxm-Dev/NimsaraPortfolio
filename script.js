b6c9772f70b8ad31af169ca11b12153142046112
N3mxaraMe-CodeArc <Nimsaraofficial@gmail.com>
2024-10-13 17:39:15 +0530

    refactor: Update landing page with improved header, about section, and portfolio images

--- script.js
+++ script.js
@@ -49,3 +49,8 @@ ScrollReveal().reveal('.home-content,.heading ,.portfolio-box2', { origin: 'top'
 ScrollReveal().reveal('.services-container ,.about-content, .contact form ,.portfolio-box5', { origin: 'bottom' });
 ScrollReveal().reveal('.portfolio-box1,.portfolio-box4', { origin: 'left' });
 ScrollReveal().reveal('.portfolio-box3,.portfolio-box6', { origin: 'right' });
+
+
+const SheduleMeet = () => {
+    window.location.href = "https://calendar.google.com/calendar/u/0/r/eventedit?vcon=meet&dates=now&hl=en&pli=1";
+}
